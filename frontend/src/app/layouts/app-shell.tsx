a1dc8bb02a29367e61b27a359641273676cd8e3e
tribeti <117581928+tribeti@users.noreply.github.com>
2026-05-28 22:15:25 +0700

    Update app-shell.tsx

--- frontend/src/app/layouts/app-shell.tsx
+++ frontend/src/app/layouts/app-shell.tsx
@@ -7,7 +7,7 @@ import { LogoMark } from "@/app/layouts/logo-mark";
 import { SideNav } from "@/app/layouts/side-nav";
 
 export function AppShellLayout() {
-
+  const [mobileOpened] = useDisclosure();
   return (
     <AppShell
       header={{ height: 60 }}
